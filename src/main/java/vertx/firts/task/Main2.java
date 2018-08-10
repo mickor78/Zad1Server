0ecdff5b6689803699c2594b80100da5310fdaa3
Michał Korzeniewski <michalkorzeniewski@macbook-pro-micha.local>
2018-08-10 10:29:56 +0200

    separate client from WebThiefServer class

---
+++ src/main/java/vertx/firts/task/Main2.java
@@ -0,0 +1,11 @@
+package vertx.firts.task;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/vertx/firts/task/WebThiefClient.java
@@ -0,0 +1,11 @@
+package vertx.firts.task;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class WebThiefClient extends AbstractVerticle {
+    @Override
+    public void start (Future<Void> fut) {
+    }
+
+}
